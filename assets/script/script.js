986ea777c81c28cd285d790ed270abdfa2639ea4
Caroline1123 <deconinck.caro@gmail.com>
2024-04-29 14:58:13 +0200

    added remove options

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,7 @@ let tasks = [];
 let id = 0;
 const addBtn = document.querySelector("#add_task");
 const ul = document.querySelector("ul");
+let selectedTasks = [];
 
 let addTask = (task) => {
     let taskText  = task.replace(/\s/g, '');
@@ -16,19 +17,49 @@ let addTask = (task) => {
     }
 }
 
+let removeTask = (task_id) => {
+    const index = tasks.findIndex(task => task.id === task_id);
+    tasks.splice(index, 1);
+}
+
+let deleteSelected = () => {
+    let checkboxItems = document.querySelectorAll(".task_checkbox:checked");
+    let selected = [...checkboxItems].map(checkboxItem => checkboxItem.name.split("_")[1]);
+    for (id of selected) {
+        removeTask(id);
+        displayTasks()
+    }
+}
+
 let displayTasks = () => {
     ul.innerHTML = "";
     for (let i = 0; i < tasks.length; i++) {
         let task = tasks[i];
         let li = document.createElement("li");
         li.innerHTML = 
-        `<input type="checkbox" name="task_${i}"/>
-        <label for="task_${id}">${task.task}</label>
-        <image class="close_button" src="assets/images/delete.svg" id="task_${id}" alt="delete task" title="Delete Task"></image>
+        `<input type="checkbox" name="task_${task.id}" class="task_checkbox"/>
+        <label for="task_${task.id}">${task.task}</label>
+        <image class="close_button" src="assets/images/delete.svg" id="task_${task.id}" alt="delete task" title="Delete Task"></image>
         `;
         ul.appendChild(li);
     }
+    const closeBtns = document.querySelectorAll(".close_button");
+    for (btn of closeBtns) {
+        btn.addEventListener("click", function() {
+            let taskId = this.id;
+            taskId = parseInt(taskId.split('_')[1]);
+            console.log(taskId);
+            removeTask(taskId);
+            displayTasks();
+        })
+    }
 }
+
+const deleteAll = document.querySelector("#delete_selected");
+deleteAll.addEventListener("click", () => {
+    deleteSelected();
+})
+
 
 addBtn.addEventListener("click", () => {
     const inputField = document.querySelector("#new_task");
@@ -42,3 +73,4 @@ addBtn.addEventListener("click", () => {
         alert("Invalid task.")
     }
 })
+
